59263113afd83e240c0cd7edd9ad38d3cd88a026
Ali Penullar <alipenullar@gmail.com>
2023-10-13 23:03:32 +0800

    axios url change

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-  baseURL: 'http://localhost:9000/api/v1/buyers',
+  baseURL: 'https://stickynote-app.onrender.com/api/v1/buyers',
   headers: {
     'Content-Type': 'application/json',
   },
